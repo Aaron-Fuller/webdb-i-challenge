0e62ea04056ad61b8621658a6fa3e3984cd93b37
Aaron-Fuller <aaron.fuller@protonmail.com>
2019-11-13 15:49:42 -0500

    Files connected and server running

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
-
 const server = express();
+const router = require('./router');
 
 server.use(express.json());
+server.use('/api/accounts', router);
 
 module.exports = server;
